ebc48fabca8958f36b5d9c6c6734f7ac1969aea4
Cory Forsstrom <cforsstrom18@gmail.com>
2021-03-02 09:48:24 -0800

    tickrs 0.14.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/tickrs.rb
+++ Formula/tickrs.rb
@@ -1,9 +1,9 @@
 class Tickrs < Formula
   desc "Realtime ticker data in your terminal 📈"
   homepage "https://github.com/tarkah/tickrs"
-  url "https://github.com/tarkah/tickrs/releases/download/v0.14.0/tickrs-v0.14.0-x86_64-apple-darwin.tar.gz"
-  sha256 "7a91779448c2945bde9a2afe1bd4868d9e277b62bcd8fb823bd6376484eb1ff6"
-  version "0.14.0"
+  url "https://github.com/tarkah/tickrs/releases/download/v0.14.1/tickrs-v0.14.1-x86_64-apple-darwin.tar.gz"
+  sha256 "de386b42e1ffac2502c6ed7ac3018ae78ee58a7cf8c07112727528f4a0bdd26b"
+  version "0.14.1"
 
   def install
     bin.install "tickrs"
